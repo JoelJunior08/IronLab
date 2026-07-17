15a664e254d8d8f3be22dc27fee7df81805827d0
JoelJunior08 <joelqsj@gmail.com>
2026-07-17 19:44:51 -0300

    Estilização e ajuste das páginas

--- Iron-Lab/src/components/Header/Header.jsx
+++ Iron-Lab/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import './Header.css'
 import logo from "../../assets/logo.png"
+import { Link } from "react-router-dom"
 
 function Header() {
 
@@ -9,14 +10,14 @@ function Header() {
                 <a>Login</a>
                 <a>Cadastre-se</a>
             </div>
-            <h1 className='cabecalho-titulo'><a>Iron</a>Lab <img src={logo} alt="Logo"/></h1>
+            <h1 className='cabecalho-titulo'><strong><a>Iron</a>Lab </strong><img src={logo} alt="Logo"/></h1>
             <p className="cabecalho-lema">Seu guia completo de exercícios para cada grupo muscular!</p>
             <div className='cabecalho-legenda'>
                 <nav>
-                    <a>Início</a>
-                    <a>Dicas de Treinos</a>
-                    <a>Sobre</a>
-                    <a>Contato</a>
+                    <Link className='link' to="/">Início</Link>
+                    <Link className='link' to="/">Dicas de Treinos</Link>
+                    <Link className='link' to="/">Sobre</Link>
+                    <Link className='link' to="/">Contato</Link>
                 </nav>
             </div>
             
